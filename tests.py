3baf7f329fed398206000018ee9a892a05d78d88
Tom Shlomi <tomshlomi7@gmail.com>
2022-11-13 01:57:31 -0500

    Added patch creation

---
+++ tests.py
@@ -0,0 +1,33 @@
+import torch
+
+def testAccuracy(model, data_loader):
+    model.eval()
+    accuracy = 0.0
+    total = 0.0
+    
+    with torch.no_grad():
+        for data in data_loader:
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            accuracy += (predicted == labels).sum().item()
+    return accuracy / total
+
+def testAccuracyByClass(model, data_loader, classes):
+    model.eval()
+    n = len(classes)
+    accuracies = [0.0] * n
+    total = [0.0] * n
+    
+    with torch.no_grad():
+        for data in data_loader:
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            for i in range(len(labels)):
+                label = labels[i]
+                pred = predicted[i]
+                total[label] += 1
+                accuracies[label] += (pred == label).item()
+    return [accuracies[i] / total[i] for i in range(n)]
